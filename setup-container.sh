b5a48aa8439cd2868c11757c413826518bff034f
Ron Olson <tachoknight@gmail.com>
2024-11-13 14:40:19 -0600

    Updated spec file name

--- setup-container.sh
+++ setup-container.sh
@@ -43,7 +43,7 @@ fi
 dnf install -y the_silver_searcher
 
 # Now get the dependencies from the spec file
-dnf builddep -y ./swift-lang.spec
+dnf builddep -y ./ssl.spec
 
 # And we always want the line numbers in vim
 echo "set nu" > ~/.vimrc
